2c0402764f96a1061e0bfbd8ca9388314f3729a8
Pedro <pedrohenrique010123456@gmail.com>
2025-07-01 18:11:37 -0300

    remove o import do card do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import styles from "./App.module.css";
-import { Card } from "./Components/Card/Card";
 import { CardGrid } from "./Components/CardGrid/CardGrid";
 import { Footer } from "./Components/Footer/Footer";
 import { Header } from "./Components/Header/Header";
