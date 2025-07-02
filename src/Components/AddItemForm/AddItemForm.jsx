6fd6964bf8bd9b6b79407d556db7ad2c00b26de0
Pedro <pedrohenrique010123456@gmail.com>
2025-07-02 20:14:19 -0300

    Estilização dos inputs

--- src/Components/AddItemForm/AddItemForm.jsx
+++ src/Components/AddItemForm/AddItemForm.jsx
@@ -1,3 +1,5 @@
+import styles from "./AddItemForm.module.css";
+
 export const AddItemForm = ({ handleSubmit, form, setForm }) => {
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -8,35 +10,40 @@ export const AddItemForm = ({ handleSubmit, form, setForm }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <div className={styles.inputContainer}>
         <label htmlFor="name">Desejo</label>
         <input
           type="text"
           name="name"
           value={form.name}
           onChange={handleChange}
+          required
         />
       </div>
-      <div>
+      <div className={styles.inputContainer}>
         <label htmlFor="description">Descrição</label>
         <input
           type="text"
           name="description"
           value={form.description}
           onChange={handleChange}
+          required
         />
       </div>
-      <div>
+      <div className={styles.inputContainer}>
         <label htmlFor="">URL Imagem</label>
         <input
           type="text"
           name="urlImage"
           value={form.urlImage}
           onChange={handleChange}
+          required
         />
       </div>
-      <button type="submit">Adicionar</button>
+      <button className={styles.button} type="submit">
+        Adicionar
+      </button>
     </form>
   );
 };
